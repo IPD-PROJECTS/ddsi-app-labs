492d60e89f0e16a688b2847a6a3d0ab8fc478d9e
Mohamed Abdoul Karim Diop <mohamedabdoulkarim.diop@pasteur.sn>
2023-12-05 13:15:18 +0000

    feat: Update feature to import list of patient

--- src/app/patient-management/pages/list-patients/list-patients.component.ts
+++ src/app/patient-management/pages/list-patients/list-patients.component.ts
@@ -1,11 +1,10 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { DialogService } from 'primeng/dynamicdialog';
 import { Table } from 'primeng/table';
 import { Patient } from 'src/app/models/patient.model';
 import { AddPatientComponent } from '../../modals/add-patients/add-patient.component';
 import { PatientService } from 'src/app/shared/service/patient/patient.service';
-import { ConfirmationService, MenuItem, MessageService } from 'primeng/api';
-import { PATIENT_FILE_LIST_MAX_SIZE } from 'src/app/shared/util';
+import { ConfirmationService, MessageService } from 'primeng/api';
 enum PATIENT_ATTRIBUTE {
   ANON_NAME = 'anon_name',
   FIRSTNAME = 'first_name',
@@ -21,7 +20,7 @@ enum PATIENT_ATTRIBUTE {
 })
 export class ListPatientsComponent {
   TABLE_COLUMN = PATIENT_ATTRIBUTE;
-  PATIENT_FILE_LIST_MAX_SIZE = PATIENT_FILE_LIST_MAX_SIZE;
+
   listPatient: Patient[] = [];
   totalRecords = 0;
   loading: boolean = true;
@@ -29,9 +28,6 @@ export class ListPatientsComponent {
   @ViewChild('filter') filter!: ElementRef;
   @ViewChild('dt1') table!: Table;
 
-  importedPatientsFile?: any;
-  uploadingPatients = false;
-  hasErrorUploadingPatients = false;
   constructor(
     private confirmService: ConfirmationService,
     private messageService: MessageService,
@@ -54,49 +50,57 @@ export class ListPatientsComponent {
       autoZIndex: true,
       data: {
         editMode,
-        patient
+        patient,
       },
-      width: '420px',
+      width: '445px',
     });
 
     ref.onClose.subscribe({
-      next:(data: {success: boolean}) => {
-        if(data?.success) {
+      next: (data: { success: boolean }) => {
+        if (data?.success) {
           this.table.reset();
-          this.messageService.add({ severity: 'success' , summary: `${editMode ? 'Edit' : 'Creation'} `, detail: 'Action is fully effective' });
+          this.messageService.add({
+            severity: 'success',
+            summary: `${editMode ? 'Edit' : 'Creation'} `,
+            detail: 'Action is fully effective',
+          });
         }
-      }
-    })
+      },
+    });
   }
 
-  onDeleteItem(event: any,item: Patient) {
+  onDeleteItem(event: any, item: Patient) {
     this.confirmService.confirm({
       target: event.target as EventTarget,
       message: 'Are you sure that you want to proceed?',
       icon: 'pi pi-exclamation-triangle',
       accept: () => {
-          this.delete(item);
-      }
-  });
+        this.delete(item);
+      },
+    });
   }
 
   delete(item: Patient) {
     this.patientService.deletePatient(item).subscribe({
-      next:(resp) => {
-        this.messageService.add({ severity: 'info', summary: 'Success', detail: `Item ${item.id} Deleted` });
+      next: (resp) => {
+        this.messageService.add({
+          severity: 'info',
+          summary: 'Success',
+          detail: `Item ${item.id} Deleted`,
+        });
         this.table.reset();
-      }
-    })
+      },
+    });
   }
 
   fetchListPatient(params: any) {
     const endpoint_params = {
       limit: params?.rows,
-      page: (params?.first / params?.rows)
-    }
+      page: params?.first / params?.rows,
+    };
     this.loading = true;
     this.patientService.getListPatients(endpoint_params).subscribe({
-      next: (resp: { count: number, results: any[]}) => {
+      next: (resp: { count: number; results: any[] }) => {
         this.loading = false;
         this.totalRecords = resp.count;
         this.listPatient = resp.results;
@@ -112,25 +116,34 @@ export class ListPatientsComponent {
     this.addPatient(editMode, item);
   }
 
-  onFileSelected(event: any) {
-    const importedPatientsFile = event?.currentFiles?.length ? event?.currentFiles[0] : null;
-    this.uploadingPatients = true;
-    this.hasErrorUploadingPatients = false;
-    this.patientService.uploadListPatientsWithFile(importedPatientsFile).subscribe({
-      next:(res) => {
-        this.uploadingPatients = false;
-        this.displayNotificationMsg(true, 'Import', 'List of Patients successfully imported')
+  opentModalImport() {
+    const ref = this.dialogService.open(AddPatientComponent, {
+      data: {
+        tabIndex: 1,
       },
-      error:(err) => {
-        this.uploadingPatients = false;
-        this.hasErrorUploadingPatients = true;
-        this.displayNotificationMsg(false, 'Failed', 'Error during import, please try again')
-
-      }
-    })
+      header: `Import list of Patients`,
+      autoZIndex: true,
+      width: '445px',
+    });
+    ref.onClose.subscribe({
+      next: (data: { success: boolean }) => {
+        if (data?.success) {
+          this.table.reset();
+          this.messageService.add({
+            severity: 'success',
+            summary: `Creation`,
+            detail: 'Action is fully effective',
+          });
+        }
+      },
+    });
   }
 
   displayNotificationMsg(success: boolean, title: string, message: string) {
-    this.messageService.add({ severity: success ? 'success' : 'error' , summary: title, detail: message });
+    this.messageService.add({
+      severity: success ? 'success' : 'error',
+      summary: title,
+      detail: message,
+    });
   }
 }
